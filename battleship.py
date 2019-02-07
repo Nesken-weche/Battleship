4705652e811e306852f6dc4c60f12d6314cdbb78
Adam Jayne <adamjayne402@gmail.com>
2019-02-07 14:09:32 -0500

    Started working on the battleship game

---
+++ battleship.py
@@ -0,0 +1,11 @@
+player = input('Enter a player name')
+
+print('Hey, ' + player)
+
+player_age = input("what is your age")
+x = int(player_age)
+
+if player_age < 10:
+    print('not eligible')
+else:
+    print('welcome')
