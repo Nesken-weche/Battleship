380f5486c9bef4ba06ce61931a5b8b92706e0760
Adam Jayne <adamjayne402@gmail.com>
2019-02-07 14:46:38 -0500

    Added character totaling

--- battleship.py
+++ battleship.py
@@ -10,7 +10,7 @@ if x < 10:
 else:
     print('welcome')
 
-character_name = ['Batman', 'Superman', 'Ironman']
+#character_name = ['Batman', 'Superman', 'Ironman']
 character_properties = {
     'Batman': {
         'Strength': 100,
@@ -34,5 +34,21 @@ character_properties = {
 
 if x > 10:
     print('choose one of these characters')
+for character in character_properties:
+    print(character)
 
-for 
+player_choice = input('Enter character name ')
+
+def total():
+    x = 0
+    x = x + character_properties[player_choice]['Strength']
+    x = x + character_properties[player_choice]['Weakness']
+    x = x + character_properties[player_choice]['Health']
+    x = x + character_properties[player_choice]['Fatality']
+    x = x / 4
+    return x
+
+
+if total() > 400:
+    print('You win!')
+
