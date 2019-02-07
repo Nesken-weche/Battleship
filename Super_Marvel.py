ace1a670a935d9153f45819bee88d717b46854bb
Adam Jayne <adamjayne402@gmail.com>
2019-02-07 15:58:03 -0500

    Added an extra layer of play

--- Super_Marvel.py
+++ Super_Marvel.py
@@ -31,27 +31,36 @@ character_properties = {
     }
 }
 
-if x > 10:
-    print('choose one of these characters')
-for character in character_properties:
-    print(character)
-
-player_choice = input('Enter character name ')
 
 answer = input('Do you think your player will defeat the master? Yes or No :')
 
-def total():
+
+def total(character):
     my_total = 0
-    for property in character_properties[player_choice]:
-        my_total += character_properties[player_choice][property]
+    for prop in character_properties[character]:
+        my_total += character_properties[character][prop]
 
     my_total /= 4
     return int(my_total)
 
+
+def game_round():
+    if x > 10:
+        print('choose one of these characters')
+    for character in character_properties:
+        print(character)
+
+    player_choice = input('Enter character name')
+    my_total = total(player_choice)
+    return (my_total, player_choice)
+
+
+
 if answer is "yes":
-    if total() > 70:
-        print(player_choice + ' win!')
-    elif total() == 70:
+    average, character_name = game_round()
+    if average > 70:
+        print(character_name + ' win!')
+    elif average == 70:
         print('Try again')
     else:
         print('You Loooooose')
